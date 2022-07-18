1dcd830aefdfaba15aeb543faedb1ab2d58fadb5
melvincwng <melvin.ng@outlook.sg>
2022-07-18 11:47:50 +0800

    Update ajaxAPICall.js

--- js/ajaxAPICall.js
+++ js/ajaxAPICall.js
@@ -30,14 +30,14 @@ $.ajax(settings)
 
       /**
        * Additional JavaScript logic implemented for bonus marks --> generate audio tags that allow the user to actually play the music (after providing a valid mp3 source url)
-       * Ideally, this music mp3 source url should come from the backend API GET endpoint. However, it's not implemented there.
-       * We can make use of various music streaming APIs (e.g. Deezer, Spotify, Youtube) to get the mp3 source url, many of which require an API_KEY before you can use them
-       * Hence there are a couple of ways we can do this:
-       *  - 1) Ideal way is to have that original API endpoint which we are calling to GET music details & just add another key containing mp3Source which has the respective URL value.
-       *  - 2) On the client, directly call the music streaming API. (However, the API_KEY will be easily exposed if we call the music API on the FE... hence not advisable, as other people may abuse the API_KEY)
+       * Ideally, this music mp3 source url should come from the backend API GET PHP endpoint. However, it's not implemented there.
+       * Hence, we can make use of various other music streaming APIs (e.g. Deezer, Spotify, Youtube) to get the mp3 source url, many of which require an API_KEY before you can use them
+       * To summarize, there are a couple of ways we can do to get a valid mp3 source url:
+       *  - 1) Ideal way is to edit that original API PHP endpoint which we are calling to get music details & just add another key containing mp3Source which has the respective URL value.
+       *  - 2) On the client, directly call the music streaming API. (However, the API_KEY will be easily exposed if we call the music API on the FE... hence not advisable, as other people may abuse the API_KEY. You can obfuscate it, but people can reverse engineer to get your API_KEY)
        *  - 3) Create a server & use it to call the music streaming API instead, then proxy back the response to client (Better approach, but creating another separate server API endpoint just for this feature can be abit cumbersome)
-       *  - 4) Use of serverless functions - e.g. Netlify functions, AWS Lambdas etc, Google Cloud Functions etc (Good approach as no need write the whole server side logic, but still a bit overkill)
-       *  - 5) Hardcoding the values on the client (Easiest way to implement, but of course not scalable - imagine if there are 100 songs..., but this approach is chosen due to time constraints)
+       *  - 4) Use of serverless functions - e.g. Netlify functions, AWS Lambdas etc, Google Cloud Functions etc (Good approach as no need write the whole server side logic, but still a bit overkill - in terms of the scope of this assignment)
+       *  - 5) Hardcoding the values on the client (Easiest way to implement, but of course not scalable - imagine if there are 100 songs... but this approach is chosen due to time constraints)
        * Listing out all available options above in case code enhancements or refactoring of the code are done in the future.
        */
       if (i === 0) {
@@ -56,7 +56,9 @@ $.ajax(settings)
           "https://cdns-preview-8.dzcdn.net/stream/c-8af9cfb9a0454481e21989618e7c5779-4.mp3";
         savedSongs.push(song);
       } else {
-        // If there are more songs added in the php endpoint in future, temporarily it's mp3Source url will be undefined aka the audio tags cannot be played (as there is no valid src url)
+        // If there are more songs added in the PHP endpoint in future:
+        //  - Temporarily it's mp3Source url will be undefined aka the audio tags cannot be played yet (as there is no valid src url)
+        //  - Then we would need to update and add in more code to this portion (link up the new songs to a valid mp3 source url - can google and find where the new songs are hosted on CDNs)
         song.mp3Source = "undefined";
         savedSongs.push(song);
       }
