b4cdf72073fb9afc50f11ed2c79015181aece487
melvincwng <melvin.ng@outlook.sg>
2022-07-22 22:52:06 +0800

    Update sw.js for offline functionality/support

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@
 const CACHE_VER = "cache-v1";
 
 // 1) During 'install' phase, we can tell the SW to open the cache and then tell it to store which specific files in the cache
-// TODO- Add various files that I want it to cache
 self.addEventListener("install", function (event) {
   console.log("Service Worker installing.");
   event.waitUntil(
@@ -64,13 +63,23 @@ self.addEventListener("activate", function (event) {
 // 3) During the last 'fetch' phase, basically what the SW is doing is that
 // It is comparing whether the files stored in the cache matches the files it is going to fetch via API calls
 // If it matches --> then just use the file from the cache (as dictated by the 'response' object)
-// Else do a fetch() api call to get the actual resource
+// Else do a fetch() api call to get the actual resource --> then save that fetched resource into the cache
 self.addEventListener("fetch", function (event) {
   console.log("Service Worker fetching.");
   console.log("Fetch:", event.request.url);
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request).then(function (response) {
+          // response object can only be used once --> hence need to clone it to save into cache for storage --> then return the original response object
+          let responseClone = response.clone();
+          caches.open(CACHE_VER).then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        })
+      );
     })
   );
 });
