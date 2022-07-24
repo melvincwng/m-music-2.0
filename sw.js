656d20011efaed3c04b8132739cc690106692643
melvincwng <melvin.ng@outlook.sg>
2022-07-24 14:18:38 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -143,9 +143,8 @@ function send_message_to_client(client, msg) {
 function send_message_to_all_clients(msg) {
   clients.matchAll().then((clients) => {
     clients.forEach((client) => {
-      send_message_to_client(client, msg).then((m) =>
-        console.log("[Service Worker] From Client:" + msg)
-      );
+      send_message_to_client(client, msg);
+      console.log("[Service Worker] From Client:" + msg);
     });
   });
 }
